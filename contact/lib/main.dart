39f39e75afaed948e1ddfa65be9c4c39f8b57663
muse9312 <muse9312@naver.com>
2023-07-21 07:14:04 +0900

    flutter create contact app

---
+++ contact/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); // 앱 시작해주세요
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
