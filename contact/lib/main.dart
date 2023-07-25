2c08966f435d8f474dcf78958e0719c92f3e656b
muse9312 <muse9312@naver.com>
2023-07-25 10:12:51 +0900

    flutter layout

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -25,27 +25,57 @@ class MyApp extends StatelessWidget {
     // ));
 
     // Scaffold
+    // return MaterialApp(
+    //     home: Scaffold(
+    //   // 앱을 상중하로 나눠주는 위젯
+    //   appBar: AppBar(
+    //       title: Text(
+    //     '앱임',
+    //   )),
+    //   body: Container(
+    //     child: Text('안녕'),
+    //   ),
+    //   bottomNavigationBar: BottomAppBar(
+    //     child: SizedBox(
+    //         height: 50,
+    //         child: Row(
+    //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //             children: [
+    //               Icon(Icons.phone),
+    //               Icon(Icons.message),
+    //               Icon(Icons.contact_page),
+    //             ])),
+    //   ),
+    // ));
+
+    // margin, padding, border, Align
+    // return MaterialApp(
+    //     home: Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('앱임'),
+    //   ),
+    //   body: Align(
+    //     alignment: Alignment.centerLeft,
+    //     child: Container(
+    //       width: double.infinity,
+    //       height: 50,
+    //       decoration: BoxDecoration(
+    //           color: Colors.blue, border: Border.all(color: Colors.black)),
+    //     ),
+    //   ),
+    // ));
+
     return MaterialApp(
-        home: Scaffold(
-      // 앱을 상중하로 나눠주는 위젯
-      appBar: AppBar(
-          title: Text(
-        '앱임',
-      )),
-      body: Container(
-        child: Text('안녕'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: SizedBox(
-            height: 50,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ])),
-      ),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("앱임"),
+          ),
+          body: SizedBox(
+              child: ElevatedButton(
+            onPressed: () {},
+            child: Text("글자"),
+            style: ButtonStyle(),
+          ))),
+    );
   }
 }
